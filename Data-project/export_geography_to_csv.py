54ea5bd3fd64d63f13957c9a14416db3d5a8e335
Hannah-Habibpour <hana.habibpor@gmail.com>
2025-10-30 14:37:00 -0400

    Add script to extract geography data from SQL Server and export to CSV

---
+++ Data-project/export_geography_to_csv.py
@@ -0,0 +1,38 @@
+from dotenv import load_dotenv
+import os
+import pandas as pd
+import pyodbc
+
+load_dotenv() 
+
+def fetch_data_from_sql():
+    
+    conn_str = (
+    f"Driver={{{os.getenv('DB_DRIVER')}}};"
+    f"Server={os.getenv('DB_SERVER')};"
+    f"Database={os.getenv('DB_NAME')};"
+    f"UID={os.getenv('DB_USER')};"
+    f"PWD={os.getenv('DB_PASSWORD')};"
+    "TrustServerCertificate=yes;"
+    )
+    
+    conn = pyodbc.connect(conn_str)
+    
+    
+    
+    with open("geography.sql", "r") as file:
+        query = file.read()
+
+    
+    df = pd.read_sql(query, conn)
+    print("✅ Connected successfully!")
+    
+    conn.close()
+    
+    
+    return df
+
+geography_df = fetch_data_from_sql()
+
+geography_df.to_csv('geography.csv', index=False)
+    
